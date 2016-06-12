8f4e5e380d2d1a4cd7773cde23b2345817481bdb
ShaelShah <shael.shah7@gmail.com>
2016-06-12 12:51:01 -0400

    Merged everything

--- BridgeToGrid/app/src/main/java/com/lasss/root/bridgetogrid/MainActivity.java
+++ BridgeToGrid/app/src/main/java/com/lasss/root/bridgetogrid/MainActivity.java
@@ -9,23 +9,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button wikipedia, finance, weather, news;
+    private ImageButton wikipedia, finance, weather, news;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        wikipedia = (Button) findViewById(R.id.wikipediaButton);
-        finance = (Button) findViewById(R.id.financeButton);
-        weather = (Button) findViewById(R.id.weatherButton);
-        news = (Button) findViewById(R.id.newsButton);
+        wikipedia = (ImageButton) findViewById(R.id.wikipediaButton);
+        finance = (ImageButton) findViewById(R.id.financeButton);
+        weather = (ImageButton) findViewById(R.id.weatherButton);
+        news = (ImageButton) findViewById(R.id.newsButton);
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
             requestPermissions();
@@ -33,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void searchWikipedia(View view) {
-
+        Intent wikiIntent = new Intent(this, WikipediaActivity.class);
+        startActivity(wikiIntent);
     }
 
     public void searchFinance(View view) {
